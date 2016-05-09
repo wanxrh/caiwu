84e87c3b4f5cf7bc84821e42adcaa8ea0865858c
lvlinlin <549663504@qq.com>
2016-05-09 21:14:18 +0800

    菜单

--- application/views/left.php
+++ application/views/left.php
@@ -150,7 +150,7 @@ $(function(){
     <span><img src="<?php echo SITE_COMMON_STATIC; ?>/images/leftico02.png" /></span>用户管理
     </div>
     <ul class="menuson">
-        <li><cite></cite><a href="gerenxinxi.php?act=mima" target="rightFrame">修改密码</a><i></i></li>
+        <li><cite></cite><a href="/get_password" target="rightFrame">修改密码</a><i></i></li>
         </ul>
     </dd>
     <dd>
